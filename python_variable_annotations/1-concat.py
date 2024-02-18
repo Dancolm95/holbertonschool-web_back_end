26c753c91fe024de371658ebf85cbc30a644cc8e
dancolm95 <dancolm95@gmail.com>
2024-02-18 10:09:17 -0800

    type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+type-annotated function concat that takes a string str1
+and a string str2 as arguments and returns a concatenated string
+Arguments: str1 = egg
+            str2 = shell
+"""
+
+
+def concat(str1: "egg", str2: "shell") -> str:
+    """Return concatenated string str1 + str2.
+    """
+    return str1 + str2
