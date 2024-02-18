525aec89160b7db2e64777ab331a1962438bc485
dancolm95 <dancolm95@gmail.com>
2024-02-18 11:54:56 -0800

    fixed file

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 function to_kv that takes a string k and an
 int OR float v as arguments and returns a tuple.
 The first element of the tuple is the string k.
-The second element is the square of the int/float v 
+The second element is the square of the int/float v
 and should be annotated as a float.
 Arguments: k:str , v:int or float
 """
