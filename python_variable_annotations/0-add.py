549899bea307ecf0802c86fe4da353e16188020a
dancolm95 <dancolm95@gmail.com>
2024-02-18 09:57:01 -0800

    fixed file

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,6 +7,6 @@ as arguments and returns their sum as a float.
 """
 
 
-def add(a : float, b : float) -> float:
+def add(a: float, b: float) -> float:
     """ Return sum a and b. """
     return a + b
