3fd34f78c0800553f2b47c8d4ab47f1d52e59ac7
dancolm95 <dancolm95@gmail.com>
2024-03-28 11:02:17 -0500

    fixed file

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -4,7 +4,20 @@ export default class Car {
         this._motor = motor;
         this._color = color;
     }
+    // Getter for the _brand atributt
+    get brand() {
+        return this._brand;
+    }
 
+    // Getter for the _motor attribute
+    get motor() {
+        return this._motor;
+    }
+
+    // Getter for the _color attribute
+    get color() {
+        return this._color;
+    }
     cloneCar() {
         const origin = this;
         return Object.assign(Object.create(Object.getPrototypeOf(origin)), {
