810ce69d6223cdc39cdd5e238f063bc6e0c527cb
dancolm95 <dancolm95@gmail.com>
2024-04-19 00:32:09 +0000

    return a promise. The parameter is a boolean

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (success) {
       resolve ({
         status: 200,
-        body: 'Succes',
+        body: "Succes",
       });
     } else {
       reject(new Error('The fake API is not working currently'));
