818b17b90cf7eb08a4bd1862f93fd5cce9b88b6b
dancolm95 <dancolm95@gmail.com>
2024-05-09 12:21:00 -0500

    fixed file

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,14 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ( { status: 200, body: 'succes'}))
-    .catch(() =>Error())
-    .finally(() => console.log('Got a response from the API'));
+    .then(() => {
+      const response = {
+        status: 200,
+        body: 'success',
+      };
+      return response;
+    })
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
