538c98ed68cbd5c4302f947b167f20865beba7c1
dancolm95 <dancolm95@gmail.com>
2024-05-09 12:18:38 -0500

    fixed file

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,12 +1,14 @@
 export default function getFullResponseFromAPI(success) {
-  const myPromise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (success) {
-      resolve(
-        { status: 200, body: 'Success' },
-      );
+      const result = {
+        status: 200,
+        body: 'Success',
+      };
+      resolve(result);
     } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
-  return myPromise;
+  return Promise
 }
